e10d8df068a7d267dff467146e30e6d8e5dde21f
hjbarbieri <hj.barbieri@gmail.com>
2015-10-23 23:15:38 +0000

    merge projects

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,23 +1,7 @@
 'use strict';
 
-angular.module('myApp', ['myApp.controllers',
-	'ui.router'])
-.config(['$stateProvider','$urlRouterProvider','$locationProvider',function($stateProvider,
-	$urlRouterProvider,$locationProvider) {
-	$locationProvider.html5Mode(true);
-	$stateProvider.state('view1', {
-		url: '/view1',
-		controller: 'Controller1',
-		templateUrl: '/app/views/view1.html'
-	}).state('view2',{
-		url: '/view2/:firstname/:lastname',
-		controller: 'Controller2',
-		templateUrl: '/app/views/view2.html',
-		resolve: {
-			names: function(){
-				return ['Misko','Vojta','Brad'];
-			}
-		}
-	});
-	$urlRouterProvider.otherwise('/view1');
+angular.module('myApp', [
+'ui.router','app.posts'
+]).run(['$state',function($state){
+    $state.go('allPosts');
 }]);
--- client/modules/posts/postModule.js
+++ client/modules/posts/postModule.js
@@ -1,1 +1,14 @@
-postModule.js
+'use strict';
+
+angular.module('app.posts',['app.posts.controllers','app.posts.services']).config(['$stateProvider','$locationProvider', function($stateProvider,$locationProvider){
+    $stateProvider.state('allPosts',{
+       url:'/posts',
+       templateUrl:'modules/posts/views/posts.html',
+       controller: 'PostController'
+    }).state('singlePost',{
+        url:'/posts/:id/:permalink',
+        templateUrl:'modules/posts/views/singlePost.html',
+        controller:'PostDetailsController'
+        
+    });
+}]);
