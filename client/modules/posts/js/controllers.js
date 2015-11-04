78cb5abb6094acebbd25d1f2d32d1333439c3366
hjbarbieri <hj.barbieri@gmail.com>
2015-11-04 19:25:05 +0000

    controllers

--- client/modules/posts/js/controllers.js
+++ client/modules/posts/js/controllers.js
@@ -1,20 +1,14 @@
 'use strict';
 
-angular.module('app.posts.controllers',[]).controller('PostController',['$scope','postService',function($scope,postService){
-    $scope.getAllPosts=function(){
-        return postService.getAll();
-    }
+angular.module('app.posts.controllers',[]).controller('PostController',['$scope','Post',function($scope,Post){
     
-    $scope.posts = $scope.getAllPosts();
+    $scope.posts = Post.query();
     
-}]).controller('PostDetailsController',['$stateParams','$state','$scope','postService', function($stateParams,$state,$scope,postService){
-    $scope.getPostById=function(id){
-        return postService.getPostById(id);
-    }
-    
+}]).controller('PostDetailsController',['$stateParams','$state','$scope','Post', function($stateParams,$state,$scope,Post){
+   
     $scope.closePost=function(){
         $state.go('allPosts');
     }
     
-    $scope.singlePost=$scope.getPostById($stateParams.id);
+    $scope.singlePost = Post.get({id:$stateParams.id});
 }]);
