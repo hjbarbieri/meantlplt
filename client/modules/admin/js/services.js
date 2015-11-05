fbcecd190d9c5ec2c83cca1f1c6382d3f2e16cec
hjbarbieri <hj.barbieri@gmail.com>
2015-11-05 14:33:23 +0000

    controller and services descrepancies

--- client/modules/admin/js/services.js
+++ client/modules/admin/js/services.js
@@ -1,15 +1,16 @@
 'use strict';
 
-angular.module('app.admin.services',[]).factory('Post', ['$resource','API_ENDPOINT', function($resource,API_ENDPOINT){
-	return $resource(API_ENDPOINT,{id: '@_id'},{
-		update:{
-			method: 'PUT'
-		}
-	}){
-		
-	};
-}]).service('popupService',['$window',function($window){
-	this.showPopup=function(message){
-		return $window.confirm(message);
-	}
-}]).value('API_ENDPOINT','http://localhost:3000/api/posts/:id');
+angular.module('app.admin.services',[]).factory('Post',['$resource','API_ENDPOINT', function($resource,API_ENDPOINT){
+    return $resource(API_ENDPOINT, {id: '@_id'},{
+        update: {
+            method: 'PUT'
+        }
+    })
+}]).service('popupService', ['$window',function($window){
+    this.showPopup=function(message){
+        return $window.confirm(message);
+    }
+}]).value('API_ENDPOINT','https://mongotest-hjbarbieri.c9.io/api/posts/:id'); 
+
+//'http://localhost:3000/api/posts/:id'
+// 'https://mongotest-hjbarbieri.c9.io/api/posts/:id'
