0fbd5248fa96890c953cfa24a3550c16ee4d0fba
H. Javier <hbarbieri@JAVIER.local>
2015-11-05 09:49:29 -0300

    http

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('app', [
-'ui.router','app.posts','app.admin'
+'ui.router','app.posts','app.admin','ngResource'
 ]).run(['$state',function($state){
     $state.go('allPosts');
 }]);
--- client/modules/admin/js/services.js
+++ client/modules/admin/js/services.js
@@ -1,3 +1,15 @@
 'use strict';
 
-angular.module('app.admin.services',[]);
+angular.module('app.admin.services',[]).factory('Post', ['$resource','API_ENDPOINT', function($resource,API_ENDPOINT){
+	return $resource(API_ENDPOINT,{id: '@_id'},{
+		update:{
+			method: 'PUT'
+		}
+	}){
+		
+	};
+}]).service('popupService',['$window',function($window){
+	this.showPopup=function(message){
+		return $window.confirm(message);
+	}
+}]).value('API_ENDPOINT','http://localhost:3000/api/posts/:id');
