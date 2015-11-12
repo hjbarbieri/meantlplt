2dc37ce49495aa881306ad0efb909abdafce7c9d
hjbarbieri <hj.barbieri@gmail.com>
2015-11-12 21:05:46 +0000

    Directives working

--- client/modules/posts/js/directives.js
+++ client/modules/posts/js/directives.js
@@ -1,15 +1,22 @@
-angular.module('app.posts.directives',[]).directive('spbComments',function(Post){
+angular.module('app.posts.directives', []).directive('spbComments', function(Post) {
     return {
         restrict: 'AEC',
-        scope:{
+        scope: {
             postInstance:'='
         },
-        replace=true,
-        link:function(scope,elem,attrs){
-            scope.saveComment=function(){
-                var postID=scope.postInstance._id,savedPostInstance={};
+        replace:true,
+        link: function(scope, elem, attrs) {
+            scope.saveComment = function() {
+                var postID = scope.postInstance._id,
+                    savedPostInstance = {};
+                scope.comment.datePublished = new Date(); //Give a date to the comment
+                angular.copy(scope.postInstance, savedPostInstance); //copy the post instance in `scope` to a variable `savedPostInstance`
+                savedPostInstance.comments.unshift(scope.comment); //push the comment to the savedPostInstance
+                scope.postInstance.comments.unshift(scope.comment); //push the comment to the `scope.postInstance` as well
+                scope.comment = {}; // clear the comment
+                savedPostInstance.$update(); //Now update `savedPostInstance` so that the new comment goes to the server.
             }
         },
-        templateUrl:'modules/posts/views/comments.html'
+        templateUrl: 'modules/posts/views/comments.html'
     }
 });
